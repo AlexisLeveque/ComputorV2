d583262881a6b1751bf978650016151953901f1b
aleveque <alex.17940@gmail.com>
2018-05-09 20:26:24 +0200

    -

--- type.py
+++ type.py
@@ -1,4 +1,11 @@
+from __future__ import division
 from math_func import ft_pow
+
+
+class Function:
+    def __init__(self, function, var):
+        self.func = function
+        self.var = var
 
 
 class Rationels:
@@ -9,47 +16,50 @@ class Rationels:
     def __str__(self):
         return str(self.nbr)
 
+    def to_str(self):
+        return str(self.nbr)
+
     def add(self, nbr):
         if isinstance(nbr, Rationels):
-            self.nbr += nbr.nbr
-            return self
+            res = self.nbr + nbr.nbr
+            return Rationels(res)
         if isinstance(nbr, Complex):
             return Complex(self.nbr + nbr.r, nbr.i)
 
     def sous(self, nbr):
         if isinstance(nbr, Rationels):
-            self.nbr -= nbr.nbr
-            return self
+            res = self.nbr - nbr.nbr
+            return Rationels(res)
         if isinstance(nbr, Complex):
             return Complex(self.nbr - nbr.r, nbr.i)
 
     def mult(self, nbr):
         if isinstance(nbr, Rationels):
-            self.nbr *= nbr.nbr
-            return self
+            res = self.nbr * nbr.nbr
+            return Rationels(res)
         if isinstance(nbr, Complex):
             return Complex(self.nbr * nbr.r, self.nbr * nbr.i)
 
     def div(self, nbr):
         if isinstance(nbr, Rationels):
-            self.nbr /= nbr.nbr
-            return self
+            res = self.nbr / nbr.nbr
+            return Rationels(res)
         if isinstance(nbr, Complex):
-            haut = Complex(nbr.r, -nbr.i).mult(self.nbr)
+            haut = Complex(nbr.r, -nbr.i).mult(self)
             bas = nbr.mult(Complex(nbr.r, -nbr.i))
             return haut.div(Rationels(bas.r))
 
     def mod(self, nbr):
         if isinstance(nbr, Rationels):
-            self.nbr %= nbr.nbr
-            return self
+            res = self.nbr % nbr.nbr
+            return Rationels(res)
         if isinstance(nbr, Complex):
             return "Error"
 
     def pow(self, nbr):
         if isinstance(nbr, Rationels):
-            self.nbr = ft_pow(self.nbr, nbr.nbr)
-            return self
+            res = ft_pow(self.nbr, nbr.nbr)
+            return Rationels(res)
         if isinstance(nbr, Complex):
             return Complex(self.nbr - nbr.r, nbr.i)
 
@@ -64,39 +74,44 @@ class Complex:
         str_i = str(self.i)
         return str_r + ' + ' + str_i + 'i '
 
+    def to_str(self):
+        str_r = str(self.r)
+        str_i = str(self.i)
+        return str_r + ' + ' + str_i + 'i '
+
     def add(self, nbr):
         if isinstance(nbr, Rationels):
-            self.r += nbr.nbr
-            return self
+            r = self.r + nbr.nbr
+            return Complex(r, self.i)
         if isinstance(nbr, Complex):
-            self.r += nbr.r
-            self.i += nbr.i
-            return self
+            r = self.r + nbr.r
+            i = self.i + nbr.i
+            return Complex(r, i)
 
     def sous(self, nbr):
         if isinstance(nbr, Rationels):
-            self.r -= nbr.nbr
-            return self
+            r = self.r - nbr.nbr
+            return Complex(r, self.i)
         if isinstance(nbr, Complex):
-            self.r -= nbr.r
-            self.i -= nbr.i
-            return self
+            r = self.r - nbr.r
+            i = self.i - nbr.i
+            return Complex(r, i)
 
     def mult(self, nbr):
         if isinstance(nbr, Rationels):
-            self.r *= nbr.nbr
-            self.i *= nbr.nbr
-            return self
+            r = self.r + nbr.nbr
+            i = self.i + nbr.nbr
+            return Complex(r, i)
         if isinstance(nbr, Complex):
-            self.r = self.r * nbr.r + self.i * nbr.i * -1
-            self.i -= self.r * nbr.i + self.i * nbr.r
-            return self
+            r = self.r * nbr.r + self.i * nbr.i * -1
+            i = self.r * nbr.i + self.i * nbr.r
+            return Complex(r, i)
 
     def div(self, nbr):
         if isinstance(nbr, Rationels):
-            self.r /= nbr.nbr
-            self.i /= nbr.nbr
-            return self
+            r = self.r / nbr.nbr
+            i = self.i / nbr.nbr
+            return Complex(r, i)
         if isinstance(nbr, Complex):
             haut = Complex(nbr.r, -nbr.i).mult(self)
             bas = Complex(nbr.r, -nbr.i).mult(nbr)
@@ -107,11 +122,16 @@ class Complex:
 
     def pow(self, nbr):
         if isinstance(nbr, Rationels):
-            base = self
-            res = self
+            base = Complex(self.r, self.i)
+            res = Complex(self.r, self.i)
             while nbr.nbr > 1:
-                res = self.mult(base)
+                res = res.mult(base)
                 nbr.nbr -= 1
             return res
         if isinstance(nbr, Complex):
             return "Error"
+
+
+class Matrice:
+    def __init__(self, nbr):
+        self.r = 0
